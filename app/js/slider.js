275a446788e9b2f32b9039f4015077cdfb3985b9
Postas <postas87@mail.ru>
2019-02-15 18:03:57 +0300

    new slider script, bug fixes

--- app/js/slider.js
+++ app/js/slider.js
@@ -1,35 +1,33 @@
-var buttons = document.querySelectorAll('.header__lower-header-slider-controls-button');
-var blocks = document.getElementsByClassName('header__lower-header-slider-content-box');
-var scrolledContainer = document.getElementsByClassName('header__lower-header-slider-content-wrapper')[0];
-var buttonPosition = 0;
+var buttons = document.getElementsByClassName('slider-controls-button');
+var blocks = document.getElementsByClassName('slider-content-box');
+var scrolledContainer = document.getElementsByClassName('slider-content-wrapper')[0];
+var autoScrollPosition = 0;
+
 for (var i = 0; i < buttons.length; i++) ((buttonNumber) => {
     buttons[i].addEventListener('click', function () {
-        clear();
-        buttonPosition = buttonNumber;
-        buttons[buttonNumber].classList.add('header__lower-header-slider-controls-button_active');
-        scrolledContainer.style.bottom = 300*(buttonNumber) + 'px';
+        slide(buttonNumber);
     })
 })(i);
 
-function clear() {
+function slide(buttonPosition) {
     for (var i = 0; i < buttons.length; i++) {
-        buttons[i].classList.remove('header__lower-header-slider-controls-button_active');
+        buttons[i].classList.remove('slider-controls-button_active');
+        blocks[i].classList.remove('slider-content-box_active');
     };
+    autoScrollPosition = buttonPosition;
+    blocks[autoScrollPosition].classList.add('slider-content-box_active');
+    buttons[autoScrollPosition].classList.add('slider-controls-button_active');
+    scrolledContainer.style.bottom = 300*(autoScrollPosition) + 'px';
+    autoScrollPosition++;
+        if (autoScrollPosition === 3 ) {
+            autoScrollPosition = 0;
+        }
 }
-
 
 function autoScroll() {
     setTimeout(function () {
-        clear();
-        buttons[buttonPosition].classList.add('header__lower-header-slider-controls-button_active');
-        console.log(buttonPosition);
-        scrolledContainer.style.bottom = 300*(buttonPosition) + 'px';
-        buttonPosition++;
-        if (buttonPosition === 3 ) {
-            buttonPosition = 0;
-        }
+        slide(autoScrollPosition);
         autoScroll();
-    }, 5000)
+    }, 5000);
 }
-
 autoScroll();
