5baf3181af435fa64526fd2840b1b34199ce71cd
Postas <postas87@mail.ru>
2019-02-16 17:27:29 +0300

    features block completed

---
+++ app/js/header_slider.js
@@ -0,0 +1,33 @@
+var headerButtons = document.getElementsByClassName('header-slider-controls-button');
+var headerBlocks = document.getElementsByClassName('header-slider-content-box');
+var scrolledContainer = document.getElementsByClassName('header-slider-content-wrapper')[0];
+var autoScrollPosition = 0;
+
+for (var i = 0; i < headerButtons.length; i++) ((buttonNumber) => {
+    headerButtons[i].addEventListener('click', function () {
+        slide(buttonNumber);
+    })
+})(i);
+
+function slide(buttonPosition) {
+    for (var i = 0; i < headerButtons.length; i++) {
+        headerButtons[i].classList.remove('header-slider-controls-button_active');
+        headerBlocks[i].classList.remove('header-slider-content-box_active');
+    };
+    autoScrollPosition = buttonPosition;
+    headerBlocks[autoScrollPosition].classList.add('header-slider-content-box_active');
+    headerButtons[autoScrollPosition].classList.add('header-slider-controls-button_active');
+    scrolledContainer.style.bottom = 300*(autoScrollPosition) + 'px';
+    autoScrollPosition++;
+        if (autoScrollPosition === 3 ) {
+            autoScrollPosition = 0;
+        }
+}
+
+// function autoScroll() {
+//     setTimeout(function () {
+//         slide(autoScrollPosition);
+//         autoScroll();
+//     }, 5000);
+// }
+// autoScroll();
