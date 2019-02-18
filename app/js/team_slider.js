ed28573f87f8cfd7fa1596740380f313fd9f9142
Postas <postas87@mail.ru>
2019-02-18 11:06:45 +0300

    team block completed

---
+++ app/js/team_slider.js
@@ -0,0 +1,36 @@
+var teamBoxes = document.getElementsByClassName('team-slider__content-box');
+// var featuresSliderContent = document.getElementsByClassName('features-slider__inner-content')[0];
+var teamButtons = document.getElementsByClassName('team-slider__controls-button');
+var teamScrolledContainer = document.getElementsByClassName('team-slider__content-wrapper')[0];
+var teamButtonPosition = 0;
+
+// featuresSliderContent.style.width = 100 * featuresBoxes.length + '%';
+
+for (var i = 0; i < teamButtons.length; i++) ((buttonNumber) => {
+    teamButtons[i].addEventListener('click', function () {
+        teamSlide(buttonNumber);
+    })
+})(i);
+
+function teamSlide(buttonPosition) {
+    for (var i = 0; i < teamButtons.length; i++) {
+        teamButtons[i].classList.remove('team-slider__controls-button_active');
+        teamBoxes[i].classList.remove('team-slider__content-box_active');
+    };
+    teamButtonPosition = buttonPosition;
+    teamBoxes[teamButtonPosition].classList.add('team-slider__content-box_active');
+    teamButtons[teamButtonPosition].classList.add('team-slider__controls-button_active');
+    teamScrolledContainer.style.transform =  'translateX(-' + 33.333*(teamButtonPosition) + '%)';
+    teamButtonPosition++;
+    if (teamButtonPosition === 3 ) {
+        teamButtonPosition = 0;
+    }
+}
+
+function teamAutoScroll() {
+    setTimeout(function () {
+        teamSlide(teamButtonPosition);
+        teamAutoScroll();
+    }, 10000);
+}
+ teamAutoScroll();
